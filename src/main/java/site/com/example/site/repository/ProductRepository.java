9218ce416ab461c081e587f48bbcb131e438bad1
Anu-sv <vshivani2410@gmail.com>
2022-04-05 23:43:54 +0530

    message

---
+++ src/main/java/site/com/example/site/repository/ProductRepository.java
@@ -0,0 +1,31 @@
+package site.com.example.site.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import site.com.example.site.model.Product;
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long>
+{
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
